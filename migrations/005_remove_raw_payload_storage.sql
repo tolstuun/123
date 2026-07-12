2bb5c778d23bff21a8a142c2d1bb01f3af38e8a7
tolstuun <tolstuun@gmail.com>
2026-07-12 23:30:17 +0200

    Recreate dependent unassigned runs view

--- migrations/005_remove_raw_payload_storage.sql
+++ migrations/005_remove_raw_payload_storage.sql
@@ -1,3 +1,9 @@
+DROP VIEW IF EXISTS unassigned_analysis_runs;
 ALTER TABLE analysis_runs DROP CONSTRAINT IF EXISTS analysis_runs_raw_payload_id_fkey;
 ALTER TABLE analysis_runs DROP COLUMN IF EXISTS raw_payload_id;
 DROP TABLE IF EXISTS raw_api_payloads;
+CREATE VIEW unassigned_analysis_runs AS
+SELECT r.*
+FROM analysis_runs r
+LEFT JOIN logical_experiment_group_runs l ON l.analysis_run_id = r.id
+WHERE l.analysis_run_id IS NULL;
